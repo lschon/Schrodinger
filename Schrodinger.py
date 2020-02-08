45540887f36635b8c76e5ff802b5ffddedf589fa
Lucas Schön <s1718492@ed.ac.uk>
2020-02-08 20:55:07 +0000

    newest

--- Schrodinger.py
+++ Schrodinger.py
@@ -5,8 +5,10 @@ pygame.init()
 
 display_width=500
 display_height=500
-catwidth=50
-catheight=50
+cat_width=50
+cat_height=50
+quartercircle_width=20
+quartercircle_height=20
 
 clock = pygame.time.Clock()
 
@@ -15,7 +17,9 @@ windowSurface = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('Schrodingers Cat')
 
 cat = pygame.image.load('cat.png')
-cat = pygame.transform.scale(cat, (catwidth,catheight))
+cat = pygame.transform.scale(cat, (cat_width, cat_height))
+quartercircle = pygame.image.load('quartercircle.jpg')
+quartercircle = pygame.image.load(quartercircle, (quartercircle_width, quartercircle_height))
 done = False
 
 
@@ -41,15 +45,16 @@ while not done:
     pressed = pygame.key.get_pressed()
     if pressed[pygame.K_UP] and y > 0:
         y -= vel
-    if pressed[pygame.K_DOWN] and y < display_height - catheight:
+    if pressed[pygame.K_DOWN] and y < display_height - cat_height:
         y += vel
     if pressed[pygame.K_LEFT] and x > 0:
         x -= vel
-    if pressed[pygame.K_RIGHT] and x < display_width - catwidth:
+    if pressed[pygame.K_RIGHT] and x < display_width - cat_width:
         x += vel
 
     windowSurface.fill(WHITE)
     windowSurface.blit(cat, (x,y))
+    windowSurface.blit(quartercircle, (0,0))
     pygame.display.update()
     pygame.display.flip()
     clock.tick(60)
