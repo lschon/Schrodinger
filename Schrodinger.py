21f7f0fe72dc5642bbffd76ad9e88569169ce5a8
josephsleiman16 <josephsleiman16@gmail.com>
2020-02-09 05:24:19 +0000

    fuckK

--- Schrodinger.py
+++ Schrodinger.py
@@ -1,10 +1,12 @@
 import pygame
 import random
+from pygame.locals import *
+import math 
 
 pygame.init()
 display_width=800
 display_height=600
-cat_width=120
+cat_width=150
 cat_height=100
 quartercircle_width=60
 quartercircle_height=60
@@ -52,7 +54,7 @@ GREY = (160,160,160)
 YELLOW = (255, 255, 50)
 background = bgs
 rate = 60
-
+speed_factor = 0.5
 font = pygame.font.Font(None, 36)
 #MAKING THE INSTRUCTION PAGE
 display_instructions = True
@@ -107,12 +109,48 @@ class Radiation:
 
 radiation=Radiation(xrad, yrad, sizerad)
 
+def random_angle(x,y):
+    anglerad=random.randint(x, y)
+    anglerad *= math.pi/180
+    return anglerad
+
+
+#particle print function, make 1 for each particle
+def particle(part_x, part_y, radius, colour):
+    pygame.draw.circle(screen, colour, (part_x, part_y), radius)
+    return part_x, part_y	
+
+
+rad1_x = 0
+rad1_y = 0
+
+rad2_x = 0
+rad2_y = display_height
+
+rad3_x = display_width
+rad3_y = 0
+
+rad4_x = display_width
+rad4_y = display_height
+
+rad_speed1 = 2
+rad_speed2 = 2
+rad_speed3 = 2
+rad_speed4 = 2
+
+rad_width = 5
+rad_traj1 = random_angle(90, 180)
+rad_traj2 = random_angle(0, 90)
+rad_traj3 = random_angle(180,270)
+rad_traj4 = random_angle(270,360)
+
 #timer
 counter, text = 0, '0'
 pygame.time.set_timer(pygame.USEREVENT, 1000)
 font = pygame.font.SysFont('comicsansms', 60)
 timer_clr=BLACK
 game_over=False
+
 #main game loop
 while not done:
     for event in pygame.event.get():
@@ -158,7 +196,11 @@ while not done:
         vel = 5
         rate = 60
         timer_clr = BLACK
+
+
     #NEED IF STATEMENT for when particle hits cat and then the counter stops and the end screen appears with counter time where DONE = TRUE & Game_over = True
+
+
     screen.blit(background, (0, 0))
     screen.blit(cat, (xcat,ycat))
     screen.blit(quartercircle1, (0,0))
@@ -166,13 +208,50 @@ while not done:
     screen.blit(quartercircle3, (0,display_height-quartercircle_height))
     screen.blit(font.render(text, True, (timer_clr)), (0.85*display_width, 0.1*display_height))
     screen.blit(quartercircle4, (display_width-quartercircle_width, display_height-quartercircle_height))
-    pygame.display.update()
-    pygame.display.flip()
-    clock.tick(rate)
+
+    print(rad1_x)
+    p1x, p1y = particle(rad1_x, rad1_y, rad_width, GREEN)
+    p2x, p2y = particle(rad2_x, rad2_y, rad_width, GREEN)
+    p3x, p3y = particle(rad3_x, rad3_y, rad_width, GREEN)
+    p4x, p4y = particle(rad4_x, rad4_y, rad_width, GREEN)
+    rad1_y += rad_speed1*math.cos(rad_traj1)
+    rad1_x += rad_speed1*math.sin(rad_traj1)
+    rad2_y += rad_speed2*math.cos(rad_traj2)
+    rad2_x += rad_speed2*math.sin(rad_traj2)
+    rad3_y += rad_speed3*math.cos(rad_traj3)
+    rad3_x += rad_speed3*math.sin(rad_traj3)
+    rad4_y += rad_speed4*math.cos(rad_traj4)
+    rad4_x += rad_speed4*math.sin(rad_traj4)
+
+    #speed factor reversing (with a negative) the particle trajectory and increasing speed
+    if (p1x < 0 or p1x > display_width or p1y < 0 or p1y > display_height):
+        rad_speed1 += speed_factor
+        rad1_x = 0
+        rad1_y = 0
+        rad_traj1 = random_angle(90, 180)
+    if (p2x < 0 or p2x > display_width or p2y < 0 or p2y > display_height):
+        rad_speed2 += speed_factor
+        rad2_x = 0
+        rad2_y = display_height
+        rad_traj2 = random_angle(90, 180)
+    if (p3x < 0 or p3x > display_width or p3y < 0 or p3y > display_height):
+        rad_speed3 += speed_factor
+        rad3_x = display_width
+        rad3_y = 0
+        rad_traj3 = random_angle(90, 180)
+    if (p4x < 0 or p4x > display_width or p4y < 0 or p4y > display_height):
+        rad_speed4 += speed_factor
+        rad4_x = display_width
+        rad4_y = display_height
+        rad_traj4 = random_angle(90, 180)
+        pygame.display.update()
+        pygame.display.flip()
+        clock.tick(rate)
+    
     #When the game ends, say that game_over=True
-    if game_over=True:
+    '''if game_over=True:
             screen.blit(background, (0, 0))
             largeFont = pygame.font.SysFont(None, 80) # creates a font object
             score = largeFont.render('Time alive: '+ str(count)+ " seconds",1, BLUE)
             screen.blit(score, display_height/2)
-            pygame.display.update()
+            pygame.display.update()'''
